b65138ddfc4e0fb01e4b3e0435e3b4e4d2d331f5
Shandy Dailami <shandydailami@gmail.com>
2025-02-10 00:25:58 +0800

    create read update delete

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -3,15 +3,133 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Product as ModelProduct;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Product extends BaseController
 {
+    public function index()
+    {
+        $model = new ModelProduct();
+        $product = $model->select('product.*, category.name')->join('category', 'category.id = product.categpory_id')->findAll();
+        $data = [
+            'title' => 'Product',
+            'products' => $product,
+        ];
+        return view('product/index', $data);
+    }
+
     public function createPage()
     {
-        return view('product/create');
+        return view('product/create', ['title' => 'Create']);
     }
 
     public function create()
+    {
+        helper('form');
+        if (
+            $this->validate([
+                'name' => [
+                    'rules' => 'required',
+                    'errors' => 'Name cannot be empty',
+                ],
+                'description' => [
+                    'rules' => 'required',
+                    'errors' => 'Description cannot be empty',
+                ],
+                'category_id' => [
+                    'rules' => 'required',
+                    'errors' => 'Category cannot be empty',
+                ],
+                'price' => [
+                    'rules' => 'required',
+                    'errors' => 'Price cannot be empty',
+                ],
+                'stock' => [
+                    'rules' => 'required',
+                    'errors' => 'Stock cannot be empty',
+                ],
+            ])
+        ) {
+            $data = [
+                'name' => $this->request->getPost('name'),
+                'description' => $this->request->getPost('description'),
+                'category_id' => $this->request->getPost('category_id'),
+                'price' => $this->request->getPost('price'),
+                'stock' => $this->request->getPost('stock'),
+            ];
+            $model = new ModelProduct();
+            $model->insert($data);
 
+            return redirect()->to('product')->with('success', 'Data successfully created');
+        } else {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+    }
+
+    public function updatePage()
+    {
+        $id = $this->request->getPost('id');
+        $model = new ModelProduct();
+        $model->find($id);
+        return view('product/update', ['title' => 'Update']);
+    }
+
+    public function update()
+    {
+        helper('form');
+        if (
+            $this->validate([
+                'name' => [
+                    'rules' => 'required',
+                    'errors' => 'Name cannot be empty',
+                ],
+                'description' => [
+                    'rules' => 'required',
+                    'errors' => 'Description cannot be empty',
+                ],
+                'category_id' => [
+                    'rules' => 'required',
+                    'errors' => 'Category cannot be empty',
+                ],
+                'price' => [
+                    'rules' => 'required',
+                    'errors' => 'Price cannot be empty',
+                ],
+                'stock' => [
+                    'rules' => 'required',
+                    'errors' => 'Stock cannot be empty',
+                ],
+            ])
+        ) {
+            $id = $this->request->getPost('id');
+            $data = [
+                'name' => $this->request->getPost('name'),
+                'description' => $this->request->getPost('description'),
+                'category_id' => $this->request->getPost('category_id'),
+                'price' => $this->request->getPost('price'),
+                'stock' => $this->request->getPost('stock'),
+            ];
+            $model = new ModelProduct();
+            $model->update($id, $data);
+
+            return redirect()->to('product')->with('success', 'Data successfully created');
+        } else {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+    }
+
+    public function delete($id)
+    {
+        $model = new ModelProduct();
+        $product = $model->find($id);
+
+        if ($product) {
+            $model->delete($id);
+
+            return redirect()->to('product')->with('success', 'Data successfully deleted');
+        } else {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Data not find');
+        }
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,37 +6,12 @@ use CodeIgniter\Model;
 
 class Product extends Model
 {
-    protected $table            = 'products';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name', 'description', 'category_id', 'price', 'stock'];
 
-    protected bool $allowEmptyInserts = false;
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function getProductByCategory($category_id)
+    {
+        return $this->where('category_id', $category_id)->findAll();
+    }
 }
